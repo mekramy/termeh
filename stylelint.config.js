3ebae4d7adaa0da91bf49d6f596717d8e385365e
Mojtaba ajamekramy <mekramy@users.noreply.github.com>
2025-04-17 20:34:17 +0330

    minor fix

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,6 +5,7 @@ module.exports = {
     rules: {
         "property-no-vendor-prefix": null,
         "at-rule-empty-line-before": null,
+        "scss/no-global-function-names": null,
         "scss/dollar-variable-empty-line-before": null,
         "scss/at-mixin-argumentless-call-parentheses": null,
     },
