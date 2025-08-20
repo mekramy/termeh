001e8bf8faff763529ba4574b38cd057b11711c5
Mojtaba ajamekramy <mekramy@users.noreply.github.com>
2025-08-20 09:34:12 +0330

    init: initialize project

---
+++ stylelint.config.js
@@ -0,0 +1,13 @@
+/** @type {import('stylelint').Config} */
+module.exports = {
+    extends: ["stylelint-config-standard-scss"],
+    plugins: ["stylelint-scss"],
+    rules: {
+        "property-no-vendor-prefix": null,
+        "at-rule-empty-line-before": null,
+        "scss/no-global-function-names": null,
+        "scss/dollar-variable-empty-line-before": null,
+        "scss/at-mixin-argumentless-call-parentheses": null,
+        "scss/double-slash-comment-empty-line-before": null,
+    },
+};
